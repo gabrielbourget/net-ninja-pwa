326273bcd7eec63d300e846aab6a86c4c051534b
Gabriel Bourget <gaboxa@syntronic.com>
2021-03-22 12:16:35 -0400

    added logic

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js")
-    .then((reg) => console.log("[app.js]: Service worker registered -> ", reg))
-    .catch((err) => console.error("[app.js]: Problem registering service worker -> ", err));
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then((reg) => console.log("[app.js]: Service worker registered -> ", reg))
+      .catch((err) => console.error("[app.js]: Problem registering service worker -> ", err));
+  });
 }
