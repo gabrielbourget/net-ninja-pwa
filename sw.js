5b537f4593cc01551b37c7fba6ad9e0ce975d778
Gabriel Bourget <gaboxa@syntronic.com>
2021-03-22 00:48:39 -0400

    Service Worker activation

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 // -> Sometimes the browser complains if you don't do this.
 const self = this;
 
+// -> Fired upon service worker installation
 self.addEventListener("install", (e) => {
   console.log("[sw.js]: Service worker succeessfuly installed.");
+});
+
+// -> Fired upon service worker activation
+self.addEventListener("activate", (e) => {
+  console.log("[sw.js]: Service worker successfully activated.");
 });
