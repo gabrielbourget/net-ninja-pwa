e26cd1567bc6231e9b6f7f2453c979c1a71b6e08
Gabriel Bourget <gaboxa@syntronic.com>
2021-03-24 18:31:30 -0400

    adding recipes

--- js/db.js
+++ js/db.js
@@ -31,3 +31,19 @@ db.collection(RECIPES_COLLECTION).onSnapshot((snapshot) => {
     }
   });
 });
+
+// -> Add a new recipe to your collection
+const form = document.querySelector("form");
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const recipe = {
+    title: form.title.value,
+    ingredients: form.ingredients.value,
+  };
+
+  db.collection(RECIPES_COLLECTION).add(recipe).catch((err) => console.log(err));
+
+  form.title.value = "";
+  form.ingredients.value = "";
+});
